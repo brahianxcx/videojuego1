071c5b5bd62dc6832742f3f6888c3eaecee949d7
DreyRo <camilojaramillo102710@gmail.com>
2026-03-08 12:58:26 -0500

    enemigos creados, me falta ahcer la meta final pero lo subo por si algo

--- Scripts/area_2d.gd
+++ Scripts/area_2d.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+@export var skin: Texture2D
+@export var custom_scale: Vector2 = Vector2(1,1)
+func _ready() -> void:
+	if skin and $Sprite2D:
+		$Sprite2D.texture = skin
+		scale=custom_scale
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("hero"):
