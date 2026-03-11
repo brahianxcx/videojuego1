a0adcd67b32ff3a26c6dc04d8245665b3731a4b4
brahianxcx <brahianser223@gmail.com>
2026-03-11 02:27:21 -0500

    mire los enemigos que agregue y ya no me acuerdo que mas hice pero revise que tal

--- Scripts/genio.gd
+++ Scripts/genio.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @export var move_speed = 300
 @export var move_dir : Vector2
-@export var detection_radius : float = 1500 # Distancia corta para que no se activen solas
+@export var detection_radius : float = 2500 # Distancia corta para que no se activen solas
 @onready var anim : AnimatedSprite2D = $AnimatedSprite2D
 var activo : bool = false
 func _ready() -> void:
@@ -18,16 +18,16 @@ func _buscar_heroe_real() -> void:
 	var nodos_hero = get_tree().get_nodes_in_group("hero")
 	
 	for nodo in nodos_hero:
-		# FILTRO: Ignora si el nodo es una abeja o si es ella misma
-		if nodo == self or "abeja" in nodo.name.to_lower():
-			continue
-			
+		
+	
 		if nodo is Node2D:
 			var distancia = global_position.distance_to(nodo.global_position)
 			if distancia < detection_radius:
 				activo = true
 				return
 
-func _on_hit_area_body_entered(body: Node2D) -> void:
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
 	if body.is_in_group("hero") and body.has_method("game_over"):
 		body.game_over()
